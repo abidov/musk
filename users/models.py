0f35a1b6d6c4943019ac9ce24cef8df13e183483
Baidoolot <bay.sonunbek@gmail.com>
2020-02-25 21:11:31 +0600

    created clients models

--- users/models.py
+++ users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    pass
+
+class Clients(models.Model):
+    name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    date = models.IntegerField(blank=True, null=True)
+    phone_number = models.IntegerField()
+    email = models.CharField(max_length=255)
+    event = models.ManyToManyField(Event)
+
+    def __str__(self):
+        return self.name
